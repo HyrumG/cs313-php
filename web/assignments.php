93a0dfbcddd485af19a8707da014a3255f96f612
Hyrum Gomez <hyrumgomez@gmail.com>
2018-05-05 17:25:23 -0600

    Created all files for prove 02

---
+++ web/assignments.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel="stylesheet" type="text/css" href="assignments.css">
+    <title>My Assignments Page</title>
+  </head>
+  
+  <body>
+<?php
+
+//  phpinfo();
+
+?>
+
+    <h1 style="text-align: center">CS 313 Assignments</h1>
+
+    <ul style="text-align: center; list-style-position: inside">
+      <li><a href="assign01.html">Assignment #01.html</a></li>
+      <li><a href="">Assignment #02.html</a></li>
+      <li><a href="">Assignment #03.html</a></li>
+      <li><a href="">Assignment #04.html</a></li>
+      <li><a href="">Assignment #05.html</a></li>
+      <li><a href="">Assignment #06.html</a></li>
+      <li><a href="">Assignment #07.html</a></li>
+      <li><a href="">Assignment #08.html</a></li>
+      <li><a href="">Assignment #09.html</a></li>
+      <li><a href="">Assignment #10.html</a></li>
+      <li><a href="">Assignment #11.html</a></li>
+      <li><a href="">Assignment #12.html</a></li>
+    </ul>
+  </body>
+</html>
--- web/index.php
+++ web/index.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
   
   <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel="stylesheet" type="text/css" href="index.css">
     <title>My Home Page</title>
   </head>
   
@@ -11,6 +15,28 @@
 //  phpinfo();
 
 ?>
-    <h1>This is my home page.</h1>
+    <div id="header">
+      <header>
+        <h1>Hyrum Gomez's Home Page.</h1>
+      </header>
+    </div>
+    
+    <div align="center">
+      <p id="welcome">Welcome to my page! Today is <?php echo date("l") . "." ?></p>
+      <a href="assignments.php" id="hw">Assignments Page</a>
+    </div>
+    <br>
+    <br>
+    
+    <div class="content">
+      <img src="formula1.jpeg" alt="Formula One race cars going around a race track.">
+      <p>I love Formula One racing. I would say that my love for Formula One came suddenly. I love racing games, mostly the Gran Turismo franchise.
+          One day I went on YouTube to check out the new game that came out at the time. While watching one of the videos, I 
+          noticed that there was video gameplay footage of Formula One. I have always been intrigued by Formula One so I decided
+          to watch it. Needless to say, I became hooked. I'm embarrassed to admit how many videos I have watched. Fast-forward a 
+          few weeks, I began to follow the real races that go on throughout the world on YouTube. Since then, I have yet to miss
+          a race related video of Formula One.
+      </p>
+    </div>
   </body>
 </html>
