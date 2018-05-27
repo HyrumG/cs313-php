c0f6b42672f4327f2d0729e5f5363d8f6ab9f0ab
Hyrum Gomez <hyrumgomez@gmail.com>
2018-05-26 22:45:04 -0600

    Main page for prove 05

--- web/prv05-phpAccess.php
+++ web/prv05-phpAccess.php
@@ -35,39 +35,39 @@ catch (PDOException $ex)
 <?php
 
 
-	$query = "SELECT u.name, d.last_changed FROM 'user' u INNER JOIN diaper d ON u.id = d.id;";
+// 	$query = "SELECT u.name, d.last_changed FROM 'user' u INNER JOIN diaper d ON u.id = d.id;";
 
 
-// SELECT u.name, d.last_changed, d.dry, d.wet, d.dirty, d.mixed FROM 'user' u INNER JOIN diaper d ON u.id = d.id;"
-	$count = 0;
+// // SELECT u.name, d.last_changed, d.dry, d.wet, d.dirty, d.mixed FROM 'user' u INNER JOIN diaper d ON u.id = d.id;"
+// 	$count = 0;
 
-	foreach ($db->query($query) as $user) {
-	$name = $user["name"];
+// 	foreach ($db->query($query) as $user) {
+// 	$name = $user["name"];
 
-	echo "<li>Name: $name</li>";	
-}
+// 	echo "<li>Name: $name</li>";	
+// }
 
-	foreach ($db->query($query) as $diaper) {
-	$last_changed = $diaper["last_changed"];
-	$dry = $diaper["dry"];
-	$wet = $diaper["wet"];
-	$dirty = $diaper["dirty"];
-	$mixed = $diaper["mixed"];
+// 	foreach ($db->query($query) as $diaper) {
+// 	$last_changed = $diaper["last_changed"];
+// 	$dry = $diaper["dry"];
+// 	$wet = $diaper["wet"];
+// 	$dirty = $diaper["dirty"];
+// 	$mixed = $diaper["mixed"];
 
-	echo "<li>Last Changed: $last_changed. Dipaer was ";
-	if ($dry)
-		echo "dry.";
-	elseif ($wet) {
-		echo "wet.";
-	}
-	elseif ($dirty) {
-		echo "dirty.";
-	}
-	elseif ($mixed) {
-		echo "mixed.";
-	}
-	echo "</li></br>";	
-}
+// 	echo "<li>Last Changed: $last_changed. Dipaer was ";
+// 	if ($dry)
+// 		echo "dry.";
+// 	elseif ($wet) {
+// 		echo "wet.";
+// 	}
+// 	elseif ($dirty) {
+// 		echo "dirty.";
+// 	}
+// 	elseif ($mixed) {
+// 		echo "mixed.";
+// 	}
+// 	echo "</li></br>";	
+// }
 
 ?>
 </body>
