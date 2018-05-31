da44be59dba7e62d6dc03336afed8193c7c0bdb6
Hyrum Gomez <hyrumgomez@gmail.com>
2018-05-31 16:03:51 -0600

    In class practice

---
+++ web/inClassPractice/courses.php
@@ -0,0 +1,46 @@
+<?php
+require_once ("dbConnect.php");
+
+$db = get_db();
+
+if (!isset($db)) {
+	die("DB connetion was not set.");
+}
+
+$query = "SELECT id, name, number FROM course";
+
+$statement = $db->prepare($query);
+
+// Bind any variables I need here...
+$statement->execute();
+
+$courses = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Courses</h1>
+
+	<ul>
+<?php  
+
+	foreach ($cour as $course) {
+		$name = $course["name"];
+		$number = $course["number"];
+
+		echo "<li>$number - $name</li>";
+	}
+
+
+?>
+	</ul>
+</body>
+</html>
