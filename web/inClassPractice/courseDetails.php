d0ea289ae5ebc173931c3f6f082756ab2bebbd9a
Hyrum Gomez <hyrumgomez@gmail.com>
2018-05-31 16:28:03 -0600

    Added display page

---
+++ web/inClassPractice/courseDetails.php
@@ -0,0 +1,35 @@
+<?php  
+
+require_once ("dbConnect.php");
+
+$courseId = htmlspecialchars($_GET["course_id"]);
+
+$db = get_db();
+
+$query = "SELECT name, number FROM course WHERE id=:id";
+
+$statement = $db->prepare($query);
+$statement->bindValue(":id", $courseId, PDO::PARAM_INT);
+
+$statement->execute();
+$row = $statement->fetch();
+
+$number = $row["number"];
+$name = $row["name"];
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php  
+	
+	echo "<h1>Showing notes for: $number - $name</h1>";
+
+?>
+</body>
+</html>
+
