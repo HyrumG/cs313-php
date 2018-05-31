4a64cacbaad60641b53fa045efa7f0fe68133c11
Hyrum Gomez <hyrumgomez@gmail.com>
2018-05-31 16:44:19 -0600

    Altered display page

---
+++ web/inClassPractice/insertNote.php
@@ -0,0 +1,22 @@
+<?php
+
+$courseId = htmlspecialchars($_POST["course_id"]);
+$date = htmlspecialchars($_POST["date"]);
+$content = htmlspecialchars($_POST["content"]);
+
+require_once ("dbConnect.php");
+
+$db = get_db();
+
+$query = "INSERT INTO note (course_id, content, date) VALUES (:courseId, :content, :date)";
+
+$statement = $db->prepare($query);
+$statement->bindValue(":courseId", $courseId, PDO::PARAM_INT);
+$statement->bindValue(":content", $content, PDO::PARAM_STR);
+$statement->bindValue(":date", $date, PDO::PARAM_STR);
+
+$statement->execute();
+
+echo "It has been inserted";
+
+?>
