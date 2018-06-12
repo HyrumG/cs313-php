89ad6ce98992660083793cc478422d3e03b7f136
Hyrum Gomez <hyrumgomez@gmail.com>
2018-06-12 14:07:39 -0600

    Changed the loop

--- web/prv05-phpAccess.php
+++ web/prv05-phpAccess.php
@@ -12,24 +12,38 @@ $db = get_db();
 	<title></title>
 </head>
 <body>	
-<?php
 
+	<h2>Fill out to view the last 10 diaper entries</h2>
+	<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+		First Name: <input type="text" name="fname">
+		Last Name: <input type="text" name="lname">
+	</form>
+
+<?php  
+	
+	$fname = $_POST['fname'];
+	$lname = $_POST['lname'];
+	$arr = array($fname, $lname);
+	echo implode(" ", $arr) . "<br>";
+?>
+
+<?php
 
 	$query = "SELECT p.name, dc.last_changed, ds.status FROM diaper_change dc INNER JOIN parent p ON dc.parent_id=p.id INNER JOIN diaper_status ds ON ds.id=dc.status_id";
 
 	$statement = $db->prepare($query);
 
-// Bind any variables I need here...
-$statement->execute();
-$diapers = $statement->fetchAll(PDO::FETCH_ASSOC);
-var_dump($diapers);
+	// Bind any variables I need here...
+	$statement->execute();
+	$diapers = $statement->fetchAll(PDO::FETCH_ASSOC);
+// var_dump($diapers);
 
 	foreach ($diapers as $parent) {
 	$name = $parent["name"];
 	$last_changed = $parent["last_changed"];
 	$status = $parent["status"];
 
-	echo "<li>Name: $name, last changed: $last_changed, status: $status</li>";	
+	echo "<li>Name: $name, last changed: $last_changed, status: $status</li><br><br>";	
 }
 
 ?>
