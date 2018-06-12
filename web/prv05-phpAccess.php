d087363f362e76f4ef6862f8c7fd6a82c573d28b
Hyrum Gomez <hyrumgomez@gmail.com>
2018-06-12 14:44:49 -0600

    Changed the loop

--- web/prv05-phpAccess.php
+++ web/prv05-phpAccess.php
@@ -30,25 +30,25 @@ $db = get_db();
 	$pName = implode(" ", $arr) . "<br>";
 	echo "<p>$pName</p><br>";
 
-	// if ($_SERVER["REQUEST_METHOD"] == "POST") {	
-	// 	$query = "SELECT p.name, dc.last_changed, ds.status FROM diaper_change dc INNER JOIN parent p ON dc.parent_id=p.id INNER JOIN diaper_status ds ON ds.id=dc.status_id WHERE p.name=:parentName";
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {	
+		$query = "SELECT p.name, dc.last_changed, ds.status FROM diaper_change dc INNER JOIN parent p ON dc.parent_id=p.id INNER JOIN diaper_status ds ON ds.id=dc.status_id WHERE p.name=:parentName";
 
-	// 	$statement = $db->prepare($query);
-	// 	$statement->bindValue(":parentName", $pName, PDO::PARAM_INT);
-	// 	// Bind any variables I need here...
-	// 	$statement->execute();
-	// 	$diapers = $statement->fetchAll(PDO::FETCH_ASSOC);
-	// // var_dump($diapers);
+		$statement = $db->prepare($query);
+		$statement->bindValue(":parentName", $pName, PDO::PARAM_INT);
+		// Bind any variables I need here...
+		$statement->execute();
+		$diapers = $statement->fetchAll(PDO::FETCH_ASSOC);
+	// var_dump($diapers);
 
-	// 	foreach ($diapers as $parent) {
-	// 	$name = $parent["name"];
-	// 	$last_changed = $parent["last_changed"];
-	// 	$status = $parent["status"];
+		foreach ($diapers as $parent) {
+		$name = $parent["name"];
+		$last_changed = $parent["last_changed"];
+		$status = $parent["status"];
 
-	// 	echo "<li>Name: $name, last changed: $last_changed, status: $status</li><br><br>";	
+		echo "<li>Name: $name, last changed: $last_changed, status: $status</li><br><br>";	
 
-	// 	}
-	// }
+		}
+	}
 ?>
 </body>
 </html>
